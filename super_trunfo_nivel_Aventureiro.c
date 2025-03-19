95a0cc8a783950a394b89e8652af5ef2f7ef7b38
AlissonJZ26 <Alissonzeni160@gmail.com>
2025-03-19 22:50:53 +0000

    super_trunfo_nivel_Aventureiro

---
+++ super_trunfo_nivel_Aventureiro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    
+     int populaçao1 = 12; 
+     int populaçao2 = 6;
+     
+      
+     if(populaçao1 > populaçao2){
+        printf("populaçao 2 Venceu...\n");
+     } else if (populaçao1 < populaçao2){
+        printf("A populaçao 1 Venceu...\n");
+     } else {
+        printf("Não houver um empate!!!...\n");
+     }
+       
+    return 0;
+}
