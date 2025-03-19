2fe6887a4e57c71f695b389960fe05b0cda2639d
AlissonJZ26 <Alissonzeni160@gmail.com>
2025-03-19 22:33:12 +0000

    super_trunfo.c

---
+++ super_trunfo _nivel_novato.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+   
+    int carta1 = 12;
+    int carta2 = 6;
+
+    if(carta1 > carta2){
+        printf("Carta 2 Venceu...\n");
+    } else{
+        printf("A carta 1 e Vencedora...\n");
+    }
+    return 0;
+}
+
+
